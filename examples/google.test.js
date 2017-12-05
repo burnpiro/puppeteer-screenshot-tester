c97a1d0064d9b57585f3f6066f7366d768233d60
burnpiro <kemalpiro@gmail.com>
2017-12-05 17:28:09 +0000

    add test result generator and saving diff it there is an error while comparing

--- examples/google.test.js
+++ examples/google.test.js
@@ -4,24 +4,37 @@ const ScreenTest = require('../src/index')
 describe('google test', () => {
   let originalTimeout
 
+  // extend default interval to 10s because some image processing might take some time
+  // we can do it beforeEach or once per test suite it's up to you
+  // if you're running that on fast computer/server probably won't need to do that
   beforeEach(function() {
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
   })
 
+  // set default interval timeout for jasmine
   afterEach(function() {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout
   })
 
   it(`check if google exists`, async () => {
+    // create ScreenshotTester with optional config
     const tester = await ScreenTest()
+
+    // setting up puppeteer
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.setViewport({width: 1920, height: 1080})
     await page.goto('https://www.google.com', { waitUntil: 'networkidle0' })
     await page.type('#lst-ib', 'Hello', { delay: 100 });
-    console.log('prepare to start test')
-    await tester(page, 'test2')
+
+    // call our tester with browser page returned by puppeteer browser
+    // second parameter is optional it's just a test name if provide that's filename
+    const result = await tester(page, 'test2')
     await browser.close()
+
+    // make assertion result is always boolean
+    expect(result).toBe(true)
+
   })
 })
--- src/index.js
+++ src/index.js
@@ -4,34 +4,49 @@ const fs = require('fs')
 const parentModule = require('parent-module')
 const path = require('path')
 
-
+// currying everywhere, that allows us to create one setup and then use tester without copying config each time
 const ScreenTestFactory = function(threshold = 0.1, includeAA = false) {
+  // get path to called directory
+  // cannot use __directory because it returns module directory instead of caller
   const folderPath = path.dirname(parentModule())
   return new Promise( resolve => {
     resolve(async (page, name = 'test') => {
-      console.log(folderPath);
+      // we need that to pixelmatch and creating diff
       const {width, height} = page.viewport()
-      // console.log(width, height)
+
+      // get existing image, might return undefined
       const oldImage = await getOldImageData(folderPath, name)
-      // console.log(typeof oldImage)
+
+      // get page object from puppeteer and create screenshot without path to receive Buffer
       const screenShot = await page.screenshot({})
-      // console.log(screenShot)
       if (oldImage !== undefined) {
+        // if there is old image we can compare it with current screenshot
         const diff = new PNG({width, height})
-        // console.log(diff)
         const testPng = PNG.sync.read(screenShot)
-        const missMatchedPixels = pixelmatch(testPng.data, oldImage.data, diff.data, width, height, {threshold});
-        console.log(missMatchedPixels)
-        // console.log(diff)
-        return missMatchedPixels > 0
+
+        // call pixelmatch with three objects and dimensions with options
+        // (newImage, oldImage, emptyDiff, widthInPX, heightInPX, options)
+        const missMatchedPixels = pixelmatch(testPng.data, oldImage.data, diff.data, width, height, {threshold, includeAA})
+
+        // missMatchedPixels is number of pixels that differs between two images
+        // 0 means that images are the same
+        if (missMatchedPixels > 0) {
+          // save diff to test folder with '-diff' postfix
+          diff.pack().pipe(fs.createWriteStream(`${folderPath}/${name}-diff.png`))
+        }
+        // returns boolean value if test passes
+        return missMatchedPixels === 0
       } else {
-        // console.log(screenShot)
+        // if there is no old image we cannot compare two images so just write existing screenshot as default image
         fs.writeFileSync(`${folderPath}/${name}.png`, screenShot)
+        console.log('There was nothing to compare, current screes saved as default')
+        return true
       }
     })
   })
 }
 
+// returns promise which resolves with undefined or PNG object
 const getOldImageData = function(folderPath, name = 'test') {
   return new Promise((resolve) => {
     fs.stat(`${folderPath}/${name}.png`, (error) => {
